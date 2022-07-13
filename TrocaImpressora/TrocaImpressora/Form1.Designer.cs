5c29ffb8e5c857e1857fac93e2d09d43a487aec5
reamaral <94121666+reamaral@users.noreply.github.com>
2022-07-12 21:28:31 -0300

    Add files via upload

---
+++ TrocaImpressora/TrocaImpressora/Form1.Designer.cs
@@ -0,0 +1,102 @@
+﻿namespace TrocaImpressora
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.rdb_zebra1 = new System.Windows.Forms.RadioButton();
+            this.rdb_zebra2 = new System.Windows.Forms.RadioButton();
+            this.btn_atualiza = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 39);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(541, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Selecione qual impressora deseja utilizar para imprimir:";
+            // 
+            // rdb_zebra1
+            // 
+            this.rdb_zebra1.AutoSize = true;
+            this.rdb_zebra1.Location = new System.Drawing.Point(17, 95);
+            this.rdb_zebra1.Name = "rdb_zebra1";
+            this.rdb_zebra1.Size = new System.Drawing.Size(118, 29);
+            this.rdb_zebra1.TabIndex = 1;
+            this.rdb_zebra1.TabStop = true;
+            this.rdb_zebra1.Text = "Printer1";
+            this.rdb_zebra1.UseVisualStyleBackColor = true;
+            // 
+            // rdb_zebra2
+            // 
+            this.rdb_zebra2.AutoSize = true;
+            this.rdb_zebra2.Location = new System.Drawing.Point(283, 95);
+            this.rdb_zebra2.Name = "rdb_zebra2";
+            this.rdb_zebra2.Size = new System.Drawing.Size(118, 29);
+            this.rdb_zebra2.TabIndex = 2;
+            this.rdb_zebra2.TabStop = true;
+            this.rdb_zebra2.Text = "Printer2";
+            this.rdb_zebra2.UseVisualStyleBackColor = true;
+            // 
+            // btn_atualiza
+            // 
+            this.btn_atualiza.Location = new System.Drawing.Point(419, 180);
+            this.btn_atualiza.Name = "btn_atualiza";
+            this.btn_atualiza.Size = new System.Drawing.Size(134, 41);
+            this.btn_atualiza.TabIndex = 3;
+            this.btn_atualiza.Text = "Atualizar";
+            this.btn_atualiza.UseVisualStyleBackColor = true;
+            this.btn_atualiza.Click += new System.EventHandler(this.btn_atualiza_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(581, 248);
+            this.Controls.Add(this.btn_atualiza);
+            this.Controls.Add(this.rdb_zebra2);
+            this.Controls.Add(this.rdb_zebra1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "TrocaImpressora";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.RadioButton rdb_zebra1;
+        private System.Windows.Forms.RadioButton rdb_zebra2;
+        private System.Windows.Forms.Button btn_atualiza;
+    }
+}
+
---
+++ TrocaImpressora/TrocaImpressora/Form1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TrocaImpressora
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        /*------------------------------------------
+         * 
+         * This program doesn't have a Try/Catch, the reason is:
+         * It wasn't developed for a high end usage, the users have absolutely no power to change any file
+         * with the exception of the pdf.
+         * 
+         * 
+         * If you adapt this code for your corporation it most likely NEEDS a Try/Catch
+         * Not adding the Try/Catch WILL make your software crash if it doesn't locate the "c:\pdf\print.bat"
+         * 
+         * -----------------------------------------
+        */
+
+        private void btn_atualiza_Click(object sender, EventArgs e)
+        {
+            //"Atualiza" means "Update", this button will update the default printer into the pdf file;
+            //This is a software that writes commands on a .bat file that will be used later for the main service
+
+            string path = @"c:\pdf\print.bat";
+            if (rdb_zebra1.Checked)
+            {
+                //First printer is selected, this means that it has to set up the default printer as the printer named Printer1
+                string instrucao = "call " + (char)34 + @"C:\Program Files (x86)\Foxit Software\Foxit PDF Reader\FoxitPDFReader.exe" + (char)34 + " -t " + (char)34 + @"c:\pdf\micropdf.pdf" + (char)34 + " Printer1";
+                System.IO.File.WriteAllText(path, instrucao + "\n" + instrucao);
+                MessageBox.Show("Impressora padrão alterada"); //Tells the user the Default printer has changed
+            } else if(rdb_zebra2.Checked)
+            {
+                //Second printer is selected, this means that it has to set up the default printer as the printer named Printer2
+                string instrucao = "call " + (char)34 + @"C:\Program Files (x86)\Foxit Software\Foxit PDF Reader\FoxitPDFReader.exe" + (char)34 + " -t " + (char)34 + @"c:\pdf\micropdf.pdf" + (char)34 + " Printer2";
+                System.IO.File.WriteAllText(path, instrucao + "\n" + instrucao);
+                MessageBox.Show("Impressora padrão alterada"); //Tells the user the Default printer has changed
+            } else
+            {
+                MessageBox.Show("Nenhuma impressora selecionada"); //Tells the user there is no Radio Button checked
+            }
+
+        }
+    }
+}
